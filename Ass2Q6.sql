994f0966a3337383c6a33f275703e4e038a091a0
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2026-02-07 13:50:24 +0530

    Add files via upload

---
+++ Ass2Q6.sql
@@ -0,0 +1,9 @@
+
+SELECT 
+    log_date,
+    CASE 
+        WHEN DATEDIFF(day, LAG(log_date) OVER (ORDER BY log_date), log_date) > 1 THEN 1 
+        ELSE 0 
+    END AS IsMissingPreviousLog
+FROM 
+    TransactionLogDates;
