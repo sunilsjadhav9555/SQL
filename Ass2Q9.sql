b4d275d6bb7941118e8bfdf213973f5ed13dc146
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2026-02-09 19:12:32 +0530

    Add files via upload

---
+++ Ass2Q9.sql
@@ -0,0 +1,16 @@
+
+select * from dbo.TravellerDetails;
+
+WITH AllTravellers AS (
+    SELECT DISTINCT TravellerName FROM TravellerDetails
+),
+AllCities AS (
+    SELECT DISTINCT CityTravelled FROM TravellerDetails
+)
+SELECT T.TravellerName, C.CityTravelled
+FROM AllTravellers T
+CROSS JOIN AllCities C
+LEFT JOIN TravellerDetails D 
+    ON T.TravellerName = D.TravellerName 
+    AND C.CityTravelled = D.CityTravelled
+WHERE D.TravellerName IS NULL;
