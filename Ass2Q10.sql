0f9babe91548ec8c70741bc762a8451a21a85fc4
Sunil Jadhav <sunilsjadhav9555@gmail.com>
2026-02-10 18:57:28 +0530

    Add files via upload

---
+++ Ass2Q10.sql
@@ -0,0 +1,10 @@
+/*
+
+Functions are designed to compute values, while Stored Procedures are designed to execute business logic and modify the database.
+
+Use a Function if you need to calculate a value (like a formatted date or a math calculation) that you want to use repeatedly inside other SELECT queries.
+
+Use a Stored Procedure if you are processing a transaction, updating records, or running a complex batch job that involves multiple steps and error handling.
+
+
+*/
